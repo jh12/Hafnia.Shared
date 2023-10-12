635715cb66c08f6b6b7df0485752de51903799e2
jh12 <jabaha35@hotmail.com>
2023-10-12 16:44:38 +0200

    Add MetadataWork dto

---
+++ src/Hafnia.DTOs/MetadataWork.cs
@@ -0,0 +1,11 @@
+﻿namespace Hafnia.DTOs;
+
+public record MetadataWork
+(
+    string Id,
+    string MetadataId,
+    string Origin,
+    bool Complete,
+    DateTime UpdatedAt,
+    string JsonData
+);
