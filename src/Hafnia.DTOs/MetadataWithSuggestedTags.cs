06478a118aaa12ac4d1de8bddd8ef4c7cb183d91
jh12 <jabaha35@hotmail.com>
2023-11-05 16:55:25 +0100

    Add metadata with tag suggestions model

---
+++ src/Hafnia.DTOs/MetadataWithSuggestedTags.cs
@@ -0,0 +1,19 @@
+﻿namespace Hafnia.DTOs;
+
+public record MetadataWithSuggestedTags
+(
+    string Id,
+    string? OriginalId,
+    Uri Uri,
+    string Title,
+    string[] Tags,
+    string[] SuggestedTags
+) : 
+Metadata
+(
+    Id,
+    OriginalId,
+    Uri,
+    Title,
+    Tags
+);
