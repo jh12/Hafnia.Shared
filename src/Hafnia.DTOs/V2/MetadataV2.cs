6956123a87ba3c673a3e6d32c81abbd6fcf8683e
jh12 <jabaha35@hotmail.com>
2023-12-11 16:13:27 +0100

    Add MetadataV2 dto

---
+++ src/Hafnia.DTOs/V2/MetadataV2.cs
@@ -0,0 +1,25 @@
+﻿namespace Hafnia.DTOs.V2;
+
+public record MetadataV2
+(
+    string Id,
+    string Title,
+    string[] Tags
+);
+
+public record MetadataSourceV2
+(
+    string Id,
+    string Uri,
+    string Title,
+    string? CreatorId,
+    string[] Tags
+);
+
+public record MetadataWithSourceV2
+(
+    string Id, 
+    string Title, 
+    string[] Tags, 
+    MetadataSourceV2 Source
+) : MetadataV2(Id, Title, Tags);
